783aa862798073c579261c4c3ca1abc1752ba620
Chirag <chirag23072000@gmail.com>
2024-12-01 00:52:48 +0530

    updated routes

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.get("/user/purchases", (req, res) => {
+    res.json({ message: "success purchases" });
+});
+
+
+app.post("/user/signin", (req, res) => {
+  res.json({ message: "success signin" });
+});
+
+
+app.post("/user/signup", (req, res) => {
+  res.json({ message: "success signup" });
+});
+
+
+app.get("/courses", (req, res) => {});
+
+
+
+app.listen(3000);
